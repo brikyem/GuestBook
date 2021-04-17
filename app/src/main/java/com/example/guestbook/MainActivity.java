d15f1438aedf18b27d53721ea9c71e847590a27b
Brian Tan <briankptan@gmail.com>
2021-04-16 18:31:58 -0700

    GuestBook Project again

---
+++ app/src/main/java/com/example/guestbook/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.guestbook;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/guestbook/ParseApplication.java
@@ -0,0 +1,18 @@
+package com.example.guestbook;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("KYSYOKHM1i4Z2Sw46xPNyqdDAALD0NtdPs6MKGwd")
+                .clientKey("JulEJ0uK4NNERfWRMFULGg1mHSBq9KJLZeX4ovyC")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
