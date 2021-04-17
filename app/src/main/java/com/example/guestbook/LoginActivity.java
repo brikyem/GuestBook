b4774421dba09a55882cef034037a657ec3dee3c
Brian Tan <briankptan@gmail.com>
2021-04-16 18:34:16 -0700

    GuestBook Project again 1

--- app/src/main/java/com/example/guestbook/LoginActivity.java
+++ app/src/main/java/com/example/guestbook/LoginActivity.java
@@ -38,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private Button btnSignUp;
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
