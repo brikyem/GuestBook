5d5ab9ceaf18741dba0529ef574538f2b7ab77b7
fooledyouonce <emilycrowl2k@gmail.com>
2021-04-27 10:34:05 -0700

    data query

--- app/src/main/java/com/example/guestbook/ParseApplication.java
+++ app/src/main/java/com/example/guestbook/ParseApplication.java
@@ -3,11 +3,15 @@ package com.example.guestbook;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("KYSYOKHM1i4Z2Sw46xPNyqdDAALD0NtdPs6MKGwd")
                 .clientKey("JulEJ0uK4NNERfWRMFULGg1mHSBq9KJLZeX4ovyC")
