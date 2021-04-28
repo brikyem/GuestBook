0d1787b18c66c0cae1262cd3a0e82320d1acd268
fooledyouonce <emilycrowl2k@gmail.com>
2021-04-28 12:41:03 -0700

    post logic

--- app/src/main/java/com/example/guestbook/TimelineActivity.java
+++ app/src/main/java/com/example/guestbook/TimelineActivity.java
@@ -42,7 +42,6 @@ public class TimelineActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 goToCompose();
-
             }
         });
     }
